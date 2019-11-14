56aa41423ba516ffa38e9e90769654bc80b5c89f
Fernando Ramírez Diosdado <43101523+FernandoDiosdado@users.noreply.github.com>
2019-11-14 12:01:54 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,19 +1,26 @@
 #include <allegro.h>
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
+#include <vector>
+
 using namespace std;
-struct Nota
+typedef struct Nota
 {
     float notas[8][3];
     int duracion[8];
-};
+}Nota;
+
 int main ()
 {
+    ofstream file;
+    vector <Nota> notaI;
+    vector <Nota> notaO;
     BITMAP *buffer,*bmp,*bmp2,*notas,*boton, *cursor;
     bool botoon=false;
     char n;
     int i=0,j=0,x=350, numero;
-    struct Nota c1;
+
     allegro_init();
     install_keyboard();
     install_mouse();
@@ -24,6 +31,7 @@ int main ()
         allegro_message("Error: inicializando sistema de sonido\n%s\n", allegro_error);
         return 1;
     }
+    set_volume(70, 70);
     buffer = create_bitmap(1000, 550);
     bmp = load_bitmap("fondo3.bmp",NULL);
     bmp2 = load_bitmap("pentagrama.bmp",NULL);
@@ -33,98 +41,99 @@ int main ()
     blit(bmp,buffer,0,0,0,0,1000,667);
     blit(bmp2,buffer,0,0,200,150,640,194);
     blit(boton,buffer,0,0,180,400,100,93);
+    blit(buffer, screen, 0, 0, 0, 0, 1000, 530);
 
+    for(i=0; i<=3; i++){
+        Nota notaU;
+        for(j=0; j<=2; j++){
+            n=readkey()&0xff;
+            if(n=='0'){
+            notaU.notas[i][j]=0;
+            }
+            else if(n=='1'){
+            notaU.notas[i][j]=1;
+            }
+            else if(n=='2'){
+            notaU.notas[i][j]=2;
+            }
+            else if(n=='3'){
+            notaU.notas[i][j]=3;
+            }
+            else if(n=='4'){
+            notaU.notas[i][j]=4;
+            }
+            else if(n=='5'){
+            notaU.notas[i][j]=5;
+            }
+            else if(n=='6'){
+            notaU.notas[i][j]=6;
+            }
+            else if(n=='7'){
+            notaU.notas[i][j]=7;
+            }
+            else if(n=='8'){
+            notaU.notas[i][j]=8;
+            }
+            else if(n=='9'){
+            notaU.notas[i][j]=9;
+            }
+        }
+        printf("\n%i %i %i \n",notaU.notas[i][0],notaU.notas[i][1],notaU.notas[i][2]);
+        notaI.push_back(notaU);
+            printf("\n%i%i%i \n",notaI[i].notas[i][0],notaI[i].notas[i][1],notaI[i].notas[i][2]);
+        file.open("C:\\Users\\lufer\\Downloads\\proyecto1-master\\notas.vict", ios::out | ios::binary);
+        file.write(reinterpret_cast<char * >(&notaI), sizeof(Nota));
+        file.close();
 
-        for(i=0; i<=7; i++){
-            for(j=0; j<=2; j++)
+        numero=notaI[i].notas[i][0]*100+notaI[i].notas[i][1]*10+notaI[i].notas[i][2];
+        cout<<numero<<endl;
+
+        if(numero>=261.6 && numero<293.6)   //do
         {
-        n=readkey()&0xff;
-        if(n=='0')
-        {
-            c1.notas[i][j]=0;
+            blit(notas,buffer,0,0,x,270,21,43);
+            x=x+30;
         }
-        else if(n=='1')
+        else if(numero>=293.6 && numero<329.6)
         {
-            c1.notas[i][j]=1;
+            blit(notas,buffer,0,0,x,260,21,43);
+            x=x+30;
         }
-        else if(n=='2')
+        else if(numero>=329.6 && numero<349.2)
         {
-            c1.notas[i][j]=2;
+            blit(notas,buffer,0,0,x,250,21,43);
+            x=x+30;
         }
-        else if(n=='3')
+        else if(numero>=349.2 && numero<391.9)
         {
-            c1.notas[i][j]=3;
+            blit(notas,buffer,0,0,x,240,21,43);
+            x=x+30;
         }
-        else if(n=='4')
+        else if(numero>=391.9 && numero<440)
         {
-            c1.notas[i][j]=4;
+            blit(notas,buffer,0,0,x,230,21,43);
+            x=x+30;
         }
-        else if(n=='5')
+        else if(numero>=440 && numero<493.8)
         {
-            c1.notas[i][j]=5;
+            blit(notas,buffer,0,0,x,220,21,43);
+            x=x+30;
         }
-        else if(n=='6')
+        else if(numero>=493.8 && numero<523.2)
         {
-            c1.notas[i][j]=6;
+            blit(notas,buffer,0,0,x,210,21,43);
+            x=x+30;
         }
-        else if(n=='7')
+        else if(numero>=523.2)
         {
-            c1.notas[i][j]=7;
+            blit(notas,buffer,0,0,x,200,21,43);
+            x=x+30;
         }
-        else if(n=='8')
-        {
-            c1.notas[i][j]=2;
-        }
-        else if(n=='9')
-        {
-            c1.notas[i][j]=9;
-        }
-    }
-    numero=c1.notas[i][0]*100+c1.notas[i][1]*10+c1.notas[i][2];
-    cout<<numero;
-    if(numero>=261.6 && numero<293.6)   //do
-    {
-        blit(notas,buffer,0,0,x,270,21,43);
-        x=x+30;
-    }
-    else if(numero>=293.6 && numero<329.6)
-    {
-        blit(notas,buffer,0,0,x,260,21,43);
-        x=x+30;
-    }
-    else if(numero>=329.6 && numero<349.2)
-    {
-        blit(notas,buffer,0,0,x,250,21,43);
-        x=x+30;
-    }
-    else if(numero>=349.2 && numero<391.9)
-    {
-        blit(notas,buffer,0,0,x,240,21,43);
-        x=x+30;
-    }
-    else if(numero>=391.9 && numero<440)
-    {
-        blit(notas,buffer,0,0,x,230,21,43);
-        x=x+30;
-    }
-    else if(numero>=440 && numero<493.8)
-    {
-        blit(notas,buffer,0,0,x,220,21,43);
-        x=x+30;
-    }
-    else if(numero>=493.8 && numero<523.2)
-    {
-        blit(notas,buffer,0,0,x,210,21,43);
-        x=x+30;
-    }
-    else if(numero>=523.2)
-    {
-        blit(notas,buffer,0,0,x,200,21,43);
-        x=x+30;
-    }
         blit(buffer, screen, 0, 0, 0, 0, 1000, 530);
 
-        }
+    }
+
+    readkey();
+    x=350;
     while(!botoon)
     {
         if(mouse_x > 178 && mouse_x < 280 && mouse_y > 400 && mouse_y < 500)
@@ -132,6 +141,58 @@ int main ()
             if(mouse_b & 1)
             {
                 botoon=true;
+                    ifstream entrada;
+    entrada.open("C:\\Users\\lufer\\Downloads\\proyecto1-master\\notas.vict", ios::in | ios::binary);
+    entrada.read(reinterpret_cast<char * >(&notaO), sizeof(Nota));
+
+    for(i=0; i<=3; i++){
+        numero=notaO[i].notas[i][0]*100+notaO[i].notas[i][1]*10+notaO[i].notas[i][2];
+        cout<<numero<<endl;
+
+        if(numero>=261.6 && numero<293.6)   //do
+        {
+            blit(notas,buffer,0,0,x,270,21,43);
+            x=x+30;
+        }
+        else if(numero>=293.6 && numero<329.6)
+        {
+            blit(notas,buffer,0,0,x,260,21,43);
+            x=x+30;
+        }
+        else if(numero>=329.6 && numero<349.2)
+        {
+            blit(notas,buffer,0,0,x,250,21,43);
+            x=x+30;
+        }
+        else if(numero>=349.2 && numero<391.9)
+        {
+            blit(notas,buffer,0,0,x,240,21,43);
+            x=x+30;
+        }
+        else if(numero>=391.9 && numero<440)
+        {
+            blit(notas,buffer,0,0,x,230,21,43);
+            x=x+30;
+        }
+        else if(numero>=440 && numero<493.8)
+        {
+            blit(notas,buffer,0,0,x,220,21,43);
+            x=x+30;
+        }
+        else if(numero>=493.8 && numero<523.2)
+        {
+            blit(notas,buffer,0,0,x,210,21,43);
+            x=x+30;
+        }
+        else if(numero>=523.2)
+        {
+            blit(notas,buffer,0,0,x,200,21,43);
+            x=x+30;
+        }
+        blit(buffer, screen, 0, 0, 0, 0, 1000, 530);
+    }
+    entrada.close();
+    readkey();
                 return 0;
             }
         }
